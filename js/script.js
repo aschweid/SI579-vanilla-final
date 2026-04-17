a69793611b63f9b4817786e8006e1f542e410752
aschweid <aschweid@umich.edu>
2026-04-16 22:35:50 -0400

    Changed html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
-// const addNumbers = (first, second) => {
-//   return first + second;
-// }
-
-// if (typeof module !== 'undefined') {
-//   module.exports = {
-//     addNumbers
-//   }
-// }
-
 document.querySelector('#button')
   .addEventListener('click', () => {
-    document.querySelector('#output').innerHTML = 'World';
+    document.querySelector('#output').textContent = 'World';
   });
 
 
